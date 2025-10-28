a939deec896c8e107d1639b884406b0aea0d2126
koh11235813 <77713907+koh11235813@users.noreply.github.com>
2025-10-28 13:57:37 +0900

    change docker-compose

--- setup.sh
+++ setup.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-# マルチアーキテクチャ対応のビルダーを作成（初回のみ）
-docker buildx create --name mybuilder --use
-docker buildx inspect --bootstrap
 
-# full-power イメージをビルドしてプッシュ
-docker buildx build --platform linux/amd64,linux/arm64 -t your-username/my-app:full-power -f full-power/Dockerfile . --push
-
-# low-power イメージをビルドしてプッシュ
-docker buildx build --platform linux/amd64,linux/arm64 -t your-username/my-app:low-power -f low-power/Dockerfile . --push
+docker-compose build
